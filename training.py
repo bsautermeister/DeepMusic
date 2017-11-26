70ca0548c660812384dcc20e33b99ba82572f03a
bsautermeister <apps@bsautermeister.de>
2017-11-26 16:40:11 +0100

    Commit last changes before reinstalling/updating OS

---
+++ training.py
@@ -0,0 +1,4 @@
+import utils.audio
+
+#conv.mp3_to_wav('dataset/mp3/Test.mp3')
+print(utils.audio.read_from_wav('dataset/binary/Test.wav'))
---
+++ utils/tfrecord.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+
+def save_example(data, label):
+    pass
+
+
+def save_sequence_example(data_list, label):
+    pass
